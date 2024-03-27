467777dc5d972a0f45e87111e026f6753293274d
zeus <sharathkottalil@gmail.com>
2024-03-28 01:01:13 +1100

    w1

--- wed/wed/src/app/app.module.ts
+++ wed/wed/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { countdownComponent } from './components/countdown/countdown.component';
+//import { countdownComponent } from './components/countdown/countdown.component';
 import { testComponent } from './components/test/test.component';
 import { CommonModule } from '@angular/common';
 @NgModule({
   imports:      [ CommonModule],
-  declarations: [ testComponent, countdownComponent ],
-  exports:      [ testComponent, countdownComponent ],
+  declarations: [ testComponent ], //countdownComponent
+  exports:      [ testComponent ], //, countdownComponent
   //bootstrap:    [ testComponent ]
 })
 export class AppModule { }
